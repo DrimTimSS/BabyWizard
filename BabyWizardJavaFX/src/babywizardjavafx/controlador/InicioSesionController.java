bbf1a9beb8943ac753f06ca084181581ed65b194
Victor Martínez <victorlateralus@ciencias.unam.mx>
2020-03-12 12:06:01 -0600

    Actualizaciones a inicio de sesión y se pueden registrar usuarios

--- BabyWizardJavaFX/src/babywizardjavafx/controlador/InicioSesionController.java
+++ BabyWizardJavaFX/src/babywizardjavafx/controlador/InicioSesionController.java
@@ -20,6 +20,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -39,6 +40,8 @@ public class InicioSesionController implements Initializable {
     private TextField usuario;
     @FXML
     private PasswordField contrasenia;
+    @FXML
+    private Label datosincorrectos;
     
     @FXML
     private void registrarUsuario(ActionEvent event) throws IOException {
@@ -55,22 +58,27 @@ public class InicioSesionController implements Initializable {
     @FXML
     private void iniciarSesion(ActionEvent event) throws IOException, SQLException {
         UsuarioController uc = new UsuarioController();
-        LinkedList<UsuarioModelo> usuarioregistro = uc.selectUsuario(usuario.getText(), "", "", "", -1);
+        LinkedList<UsuarioModelo> usuarioregistro = new LinkedList<>();
+        if(!usuario.getText().equals("")){
+            usuarioregistro = uc.selectUsuario(usuario.getText(), "", "", "", -1);
+        }
         String contrareal="";
         if(usuarioregistro.size()>0){
             contrareal = usuarioregistro.get(0).getContrasenia();
-        }
-        if(contrareal == null ? contrasenia.getText() == null : contrareal.equals(contrasenia.getText())){
-            Parent loadMenuPrincipal = FXMLLoader.load(getClass().getResource("/babywizardjavafx/vista/MenuPrincipal.fxml"));
-            Scene menuPrincipalScene = new Scene(loadMenuPrincipal);
+            if((contrareal == null || contrasenia.getText().equals("")) ? contrasenia.getText() == null : contrareal.equals(contrasenia.getText())){
+                Parent loadMenuPrincipal = FXMLLoader.load(getClass().getResource("/babywizardjavafx/vista/MenuPrincipal.fxml"));
+                Scene menuPrincipalScene = new Scene(loadMenuPrincipal);
         
-            Stage mainWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            mainWindow.setScene(menuPrincipalScene);
-            mainWindow.show();
+                Stage mainWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                mainWindow.setScene(menuPrincipalScene);
+                mainWindow.show();
+            } else {
+                datosincorrectos.setText("¡Contraseña incorrecta!");
+            }
         } else {
-        
+            datosincorrectos.setText("¡Usuario no reconocido!");
         }
-        
+        contrasenia.clear();
     }
     
     @Override
--- BabyWizardJavaFX/src/babywizardjavafx/controlador/RegistroController.java
+++ BabyWizardJavaFX/src/babywizardjavafx/controlador/RegistroController.java
@@ -5,31 +5,94 @@
  */
 package babywizardjavafx.controlador;
 
+import babywizardjavafx.modelo.UsuarioModelo;
 import java.net.URL;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author valdo
+ * @author Vicaris
  */
 public class RegistroController implements Initializable {
 
-    
     @FXML
-    private Button jejesito;
+    private Button registrar;
+    @FXML
+    private TextField usuario;
+    @FXML
+    private TextField nombre;
+    @FXML
+    private TextField apellidop;
+    @FXML
+    private TextField apellidom;
+    @FXML
+    private PasswordField repetircon;
+    @FXML
+    private PasswordField contrasenia;
+    @FXML
+    private TextField usuarioadm;
+    @FXML
+    private PasswordField contraseniaadm;
+    @FXML
+    private Label datosincorrectos;
+    @FXML
+    private Label errorusuario;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
-    public void findUserAndPassword(String user){
-        
     }
+    
+    @FXML
+    public void registrar(ActionEvent event) throws SQLException{
+        boolean permisocrear = false;
+        UsuarioController uc = new UsuarioController();
+        LinkedList<UsuarioModelo> usuarioregistro = new LinkedList<>();
+        if(!usuarioadm.getText().equals("")){
+            usuarioregistro = uc.selectUsuario(usuarioadm.getText(), "", "", "", -1);
+        }
+        String contrareal="";
+        if(usuarioregistro.size()>0){
+            contrareal = usuarioregistro.get(0).getContrasenia();
+        } else {
+            datosincorrectos.setText("Usuario no reconocido.");
+        }
+        if(contrareal.equals(contraseniaadm.getText())){
+            permisocrear = true;
+        } else {
+            datosincorrectos.setText("Contraseña incorrecta.");
+        }
+        if(!permisocrear){
+            contrasenia.clear();
+        } else {
+            if(!(usuario.getText().equals("")) && !(nombre.getText().equals("")) && !(apellidop.getText().equals("")) && !(apellidom.getText().equals("")) && !(contrasenia.getText().equals(""))){
+                if(contrasenia.getText().equals(repetircon.getText())) {
+                UsuarioModelo um = new UsuarioModelo(usuario.getText(),nombre.getText(),apellidop.getText(),apellidom.getText(),contrasenia.getText(),0);
+                try{
+                    uc.createUsuario(um);
+                    Stage actualWindow = (Stage) errorusuario.getScene().getWindow();
+                    actualWindow.close();
+                } catch (SQLIntegrityConstraintViolationException e) {
+                    errorusuario.setText("¡No se pudo crear nuevo usuario!");
+                }
+                }
+            }
+        }
+    }
+    
 }
