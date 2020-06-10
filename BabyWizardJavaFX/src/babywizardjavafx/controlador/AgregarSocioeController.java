f7bcefcf0fbf78e8092c82085b512e32a0d0f27d
Sebastián Ávila Valdovinos <30665426+SebVald@users.noreply.github.com>
2020-06-09 21:59:35 -0500

    Me falla una cosa en SQL

--- BabyWizardJavaFX/src/babywizardjavafx/controlador/AgregarSocioeController.java
+++ BabyWizardJavaFX/src/babywizardjavafx/controlador/AgregarSocioeController.java
@@ -14,8 +14,16 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import babywizardjavafx.modelo.SocioeconomicoModelo;
+import java.io.IOException;
 import java.sql.SQLException;
 import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.Style;
 
 /**
  * FXML Controller class
@@ -52,7 +60,7 @@ public class AgregarSocioeController implements Initializable {
     }
     
     @FXML
-    private void agregar(ActionEvent event) throws SQLException {
+    private void agregar(ActionEvent event) throws SQLException, IOException {
         String puntaje = puntajecrudo.getText();
         String n = nse.getText();
         
@@ -64,7 +72,20 @@ public class AgregarSocioeController implements Initializable {
             return;            
         }
         
-        
+        Stage actualWindow = (Stage) puntajecrudo.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/babywizardjavafx/vista/CreadoExitosamente.fxml"));
+        Parent root = (Parent) loader.load();
+        JMetro jmetro = new JMetro(Style.LIGHT);
+        jmetro.setParent(root);
+        CreadoExitosamenteController cec = loader.getController();
+        cec.queEsCreado("Información socioeconómica agregada exitosamente.");
+        Scene exito = new Scene(root);
+        actualWindow.setScene(exito);
+        Image image = new Image("/babywizardjavafx/vista/imagenes/bwlogo.jpg");
+        actualWindow.getIcons().add(image);
+        actualWindow.setTitle("Exito");
+        actualWindow.show();
+        actualWindow.centerOnScreen();        
     }
     
 }
