6bacc16ed2a0b56d853f6d952ecfbb4b8355891f
Victor Martínez <victorlateralus@ciencias.unam.mx>
2020-11-18 16:05:55 -0600

    Application running with exe

--- BabyWizardJavaFX/src/babywizardjavafx/modelo/JdbConnection.java
+++ BabyWizardJavaFX/src/babywizardjavafx/modelo/JdbConnection.java
@@ -8,22 +8,27 @@ package babywizardjavafx.modelo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javafx.scene.control.Alert;
+import babywizardjavafx.BabyWizardJavaFX;
 
 /**
- *
+ *String jdbcURL = "jdbc:mysql://localhost:7050/babywizard?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String username = "root";
+		String password = "Ps1c0LabeS4v3r2";
  * @author Vicaris
  */
 public class JdbConnection {
     @SuppressWarnings("CallToPrintStackTrace")
     public Connection getConnection() {
-		String jdbcURL = "jdbc:mysql://localhost:3306/babywizard?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String jdbcURL = "jdbc:mysql://localhost:7050/babywizard?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		String username = "root";
-		String password = "1234";
+		String password = "Ps1c0LabeS4v3r2";
 		Connection con = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con=DriverManager.getConnection(jdbcURL, username, password);		
 		} catch(ClassNotFoundException | SQLException e) {
+                        
 			e.printStackTrace();
 		}
 		return con;
